79ba66b8e5cb68883faa02d8a6d416cc67bc15e3
andresflorez0799 <andresflorez0799@gmail.com>
2024-03-22 22:50:57 -0500

    Update

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     site: "https://andresflorez0799.github.io",
-    base: "portfolio.io",
+    base: "/portfolio.io",
 });
