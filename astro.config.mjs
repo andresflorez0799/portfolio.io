a1e228c83145584c42c57abf08d96c22d2122a16
Mairon Florez <andresflorez0799@gmail.com>
2025-05-04 20:58:38 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from "astro/config";
-import staticAdapter from "@astrojs/adapter-static";
 
 export default defineConfig({
   site: "https://andresflorez0799.github.io/portfolio.io",
-  base: "/portfolio.io/",
-  adapter: staticAdapter(),
+  base: "/portfolio.io/"
 });
