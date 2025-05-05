b0ac5e5defbe981d6ef5063173dc0bc62819a5e7
Mairon Florez <andresflorez0799@gmail.com>
2025-05-04 21:10:46 -0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,6 @@
 import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: "https://andresflorez0799.github.io/portfolio.io",
-  base: "/portfolio.io/"
+  site: "https://andresflorez0799.github.io",
+  base: "/portfolio.io"
 });
